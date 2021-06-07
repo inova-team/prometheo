395ecb2b184d4ab73e23f65e5d5499f6efa6629d
BAUNET\carango <carango@baufest.com>
2021-06-07 13:14:45 -0500

    Modificación de proyecto

--- wp-config.php
+++ wp-config.php
@@ -17,19 +17,19 @@
  *
  * @package WordPress
  */
-define('WP_ALLOW_REPAIR', true);
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'freedbtech_prometheocda' );
+define( 'DB_NAME', 'bd_prueba3' );
 
 /** MySQL database username */
-define( 'DB_USER', 'freedbtech_inovaprometheo' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Inova.2020' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'freedb.tech' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
