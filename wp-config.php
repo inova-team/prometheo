42462fd869e3742b7663118b7dcecb740ebe8256
BAUNET\carango <carango@baufest.com>
2021-06-12 00:52:47 -0500

    Jannah con licencia

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'db_prueba3' );
+define( 'DB_NAME', 'bd_prometheo' );
 
 /** MySQL database username */
 define( 'DB_USER', 'admin' );
